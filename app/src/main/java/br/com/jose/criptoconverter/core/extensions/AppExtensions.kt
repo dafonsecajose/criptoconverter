9b42aa7f9db386c6fa141317c13b36fb308c49e2
José D'Afonseca <josepfj1@gmail.com>
2021-10-19 12:12:43 -0300

    extension AppExtensions

--- app/src/main/java/br/com/jose/criptoconverter/core/extensions/AppExtensions.kt
+++ app/src/main/java/br/com/jose/criptoconverter/core/extensions/AppExtensions.kt
@@ -1,4 +1,16 @@
 package br.com.jose.criptoconverter.core.extensions
 
-class AppExtensions {
+import android.app.Activity
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import java.text.NumberFormat
+import java.util.*
+
+fun View.hideSoftKeyboard() {
+    val inm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+fun Double.formatCurrency(locale: Locale = Locale.getDefault()): String {
+    return NumberFormat.getCurrencyInstance(locale).format(this)
 }
